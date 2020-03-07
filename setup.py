8451041e2d20a3ad48544b69c7389ef0fc4d1524
Yang Tian <tianyang@wejoydata.com>
2020-03-07 21:12:45 +0800

    add example

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Author: Steven Field
+from setuptools import setup
+
+setup(
+    name="summergreen",
+    version="0.0.1",
+    author="Steven Field",
+    packages=["summergreen"],
+)
