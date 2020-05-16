96eab318a1bf5111ba59f2bd2b9d8f60070566d3
Yang Tian <tianyang@wejoydata.com>
2020-05-17 02:25:50 +0800

    add pause function if not in trading date

--- summergreen/server.py
+++ summergreen/server.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Author: Steven Field
+
 from flask import Flask
 from flask import jsonify
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -8,21 +9,32 @@ import quotation
 from stock_merger import StockMerger
 import datetime
 import threading
+import time
 
 app = Flask(__name__)
-
 app.sched = BackgroundScheduler()
+quotation.update_stock_codes()
+app.fetcher = quotation.Sina()
 app.trade_date = datetime.datetime.now().date()
 app.sm = StockMerger(app.trade_date)
-app.fetcher = quotation.Sina()
 
 
 def initialize():
-    app.fetcher = quotation.Sina()
+    """
+    initialize the fetcher and merger in beginning of every day.
+    """
+    # update stock code before initialize quotation fetcher
     quotation.update_stock_codes()
+    # initialize quotation fetcher
+    app.fetcher = quotation.Sina()
+    # initialize stock merger for gpu calculation
     app.trade_date = datetime.datetime.now().date()
     app.sm = StockMerger(app.trade_date)
-    threading.Thread(target=run_tmp_merger, daemon=True).start()
+    # pause scheduler for 6 hours if current is not trading date
+    if not app.sm.estimate_trading(app.fetcher.market_snapshot()):
+        app.sched.pause()
+        time.sleep(60 * 60 * 6)
+        app.sched.resume()
 
 
 def run_fetcher():
@@ -49,15 +61,19 @@ def server_check():
 
 @app.route('/get_stock_data', methods=['GET', 'POST'])
 def get_stock_data():
-    return jsonify({'message': f'{app.sm.stock_queue.qsize()}|||{app.sm.persistent_cdf.shape}'})
+    return jsonify({'message': f'{app.sched.state}'})
 
+
+# # test scheduler
+# app.sched.add_job(initialize, 'cron',
+#                   hour='2', minute='16', max_instances=1, second='30')
+#
+# app.sched.add_job(run_fetcher, 'cron',
+#                   max_instances=1, second='*')
 
 # initialize scheduler
 app.sched.add_job(initialize, 'cron',
-                  hour='9', minute='14', max_instances=1)
-
-app.sched.add_job(run_fetcher, 'cron',
-                  max_instances=10, second='*')
+                  hour='9', minute='14', max_instances=1, second='30')
 
 # fetcher scheduler
 app.sched.add_job(run_fetcher, 'cron',
@@ -68,18 +84,6 @@ app.sched.add_job(run_fetcher, 'cron',
                   hour='11', minute='0-31', max_instances=10, second='*')
 app.sched.add_job(run_fetcher, 'cron',
                   hour='15', minute='0', max_instances=10, second='*')
-
-threading.Thread(target=run_tmp_merger, daemon=True).start()
-
-# # tmp merger scheduler
-# app.sched.add_job(run_tmp_merger, 'cron',
-#                   hour='9', minute='15-59', max_instances=1, second='*')
-# app.sched.add_job(run_tmp_merger, 'cron',
-#                   hour='10,13-14', max_instances=1, second='*')
-# app.sched.add_job(run_tmp_merger, 'cron',
-#                   hour='11', minute='0-31', max_instances=1, second='*')
-# app.sched.add_job(run_tmp_merger, 'cron',
-#                   hour='15', minute='0', max_instances=1, second='*')
 
 # merger scheduler
 app.sched.add_job(run_merger, 'cron',
@@ -101,4 +105,5 @@ app.sched.start()
 atexit.register(lambda: app.sched.shutdown())
 
 if __name__ == '__main__':
+    threading.Thread(target=run_tmp_merger, daemon=True).start()
     app.run(debug=False, host='0.0.0.0', port=5001)
--- summergreen/stock_merger.py
+++ summergreen/stock_merger.py
@@ -5,14 +5,13 @@ import cudf
 import datetime
 import pandas as pd
 from queue import Queue
-import threading
+
 cudf.set_allocator("managed")
 
 
 class StockMerger(object):
     def __init__(self, trade_date):
         self.trade_date = trade_date
-        self.is_trading = True
         self.cut_off_time = self.trade_date
         self.shift_timedelta = datetime.timedelta(seconds=15)
         self.ordered_columns = [
@@ -25,18 +24,29 @@ class StockMerger(object):
         self.persistent_cdf = self.persistent_cdf.set_index(self.ordered_index)
         self.tmp_df = pd.DataFrame(columns=self.ordered_columns)
         self.stock_queue = Queue()
-        # self.tmp_merge_threader = threading.Thread(target=self.stock_dict2tmp_dict, daemon=True)
-        # self.tmp_merge_threader.start()
 
     def estimate_trading(self, stock_dict):
+        """
+        Get snapshot of stock and determine if is trading today.
+        :rtype: Boolean
+        :param stock_dict: stock dict snapshot
+        """
         trade_date_list = list(set([i[1].date() for i in stock_dict.keys()]))
-        if self.trade_date.date() not in trade_date_list:
-            self.is_trading = False
+        if self.trade_date not in trade_date_list:
+            return False
+        else:
+            return True
 
     def cache_stock_dict(self, stock_dict):
+        """
+        Get a snapshot of stock by fetcher to stock queue
+        """
         self.stock_queue.put(stock_dict)
 
     def stock_dict2tmp_dict(self):
+        """
+        Merge snapshot of stock in queue to tmp_df if exiting in queue
+        """
         while True:
             try:
                 stock_df_list = [self.tmp_df, pd.DataFrame.from_dict(self.stock_queue.get(), orient='index')]
@@ -50,6 +60,9 @@ class StockMerger(object):
                 print(e)
 
     def tmp2persistent_delayed(self):
+        """
+        Merge tmp_df to cudf frame and update cut off time
+        """
         tmp_df = self.tmp_df.copy()
         cut_off_time = tmp_df.index.map(lambda x: x[1]).max() - self.shift_timedelta
         tmp_df = tmp_df[tmp_df.index.map(lambda x: x[1] < cut_off_time)]
@@ -58,9 +71,15 @@ class StockMerger(object):
         self.cut_off_time = cut_off_time
 
     def tmp2persistent(self):
+        """
+        merge tmp_df to tmp_cdf
+        """
         tmp_cdf = cudf.from_pandas(self.tmp_df)
         self.persistent_cdf = cudf.concat([self.persistent_cdf, tmp_cdf])
         self.tmp_df = pd.DataFrame(columns=self.ordered_columns)
 
     def save_persistent2parquet(self, parquet_path):
+        """
+        save the tmp_cdf to parquet_path
+        """
         self.persistent_cdf.to_parquet(parquet_path)
