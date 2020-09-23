8a1710a9d551d2b04b6d36a6a338ab4b0763ec62
Yang Tian <tianyang@wejoydata.com>
2020-09-24 00:49:10 +0800

    remove server and add schedulers

--- summergreen/schedulers/fetcherscheduler.py
+++ summergreen/schedulers/fetcherscheduler.py
@@ -1,51 +1,50 @@
 # -*- coding: utf-8 -*-
 # Author: Steven Field
-from apscheduler.schedulers.background import BackgroundScheduler
-import atexit
+from apscheduler.schedulers.blocking import BlockingScheduler
 from summergreen.fetchers import sinastockfetcher
+import os
 
+if __name__ == "__main__":
+    sf = sinastockfetcher.SinaStockFetcher(
+        redis_host="localhost",
+        redis_port=6378,
+        redis_db=1,
+    )
+    bs = BlockingScheduler()
 
-class FetcherScheduler:
-    def __init__(self):
-        self.sf = sinastockfetcher.SinaStockFetcher(
-            redis_host="localhost",
-            redis_port=6378,
-            redis_db=1,
-        )
-        self.bs = BackgroundScheduler()
+    bs.add_job(
+        sf.day_initialization,
+        "cron",
+        hour="9",
+        minute="14",
+        max_instances=1,
+        second="30",
+    )
 
-        self.bs.add_job(
-            self.sf.day_initialization,
-            "cron",
-            hour="9",
-            minute="14",
-            max_instances=1,
-            second="30",
-        )
+    bs.add_job(sf.snap2redis, "cron", hour="9-10,13-14", max_instances=10, second="*")
+    bs.add_job(
+        sf.snap2redis,
+        "cron",
+        hour="11",
+        minute="0-31",
+        max_instances=10,
+        second="*",
+    )
+    bs.add_job(
+        sf.snap2redis,
+        "cron",
+        hour="15",
+        minute="0",
+        max_instances=10,
+        second="*",
+    )
 
-        self.bs.add_job(
-            self.sf.snap2redis, "cron", hour="9-10,13-14", max_instances=10, second="*"
-        )
-        self.bs.add_job(
-            self.sf.snap2redis,
-            "cron",
-            hour="11",
-            minute="0-31",
-            max_instances=10,
-            second="*",
-        )
-        self.bs.add_job(
-            self.sf.snap2redis,
-            "cron",
-            hour="15",
-            minute="0",
-            max_instances=10,
-            second="*",
-        )
+    # test samples
+    # bs.add_job(sf.snap2redis, 'cron', max_instances=10, second='*')
 
-        # test samples
-        # self.bs.add_job(self.sf.snap2redis, 'cron', max_instances=10, second='*')
+    print("Press Ctrl+{0} to exit".format("Break" if os.name == "nt" else "C"))
 
-        self.bs.start()
-        # shut down the scheduler when exiting the app
-        atexit.register(lambda: self.bs.shutdown())
+    try:
+        bs.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
--- summergreen/schedulers/loaderscheduler.py
+++ summergreen/schedulers/loaderscheduler.py
@@ -1,29 +1,28 @@
 # -*- coding: utf-8 -*-
-# Author: Steven Field
-from apscheduler.schedulers.background import BackgroundScheduler
-import atexit
+from apscheduler.schedulers.blocking import BlockingScheduler
 from summergreen.loaders import sinastockloader
+import os
 
+if __name__ == "__main__":
+    sl = sinastockloader.SinaStockLoader(
+        redis_host="localhost",
+        redis_port=6377,
+        redis_db=1,
+    )
+    bs = BlockingScheduler()
 
-class LoaderScheduler:
-    def __init__(self):
-        self.sl = sinastockloader.SinaStockLoader(
-            redis_host="localhost",
-            redis_port=6377,
-            redis_db=1,
-        )
-        self.bs = BackgroundScheduler()
+    bs.add_job(
+        sl.redis2df_today,
+        "cron",
+        hour="23",
+        minute="1",
+        second="1",
+        max_instances=1,
+    )
 
-        self.bs.add_job(
-            self.sl.redis2df_today().to_parquet,
-            "cron",
-            ["/mnt/stock_data/"],
-            hour="23",
-            minute="1",
-            second="1",
-            max_instances=1,
-        )
+    print("Press Ctrl+{0} to exit".format("Break" if os.name == "nt" else "C"))
 
-        self.bs.start()
-        # shut down the scheduler when exiting the app
-        atexit.register(lambda: self.bs.shutdown())
+    try:
+        bs.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
