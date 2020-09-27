9f3584aa24090ecd6583979158585d0d55a7f64f
Yang Tian <tianyang@wejoydata.com>
2020-09-28 00:28:22 +0800

    change schedulers and rename analyser class name

--- summergreen/analysers/ranger.py
+++ summergreen/analysers/ranger.py
@@ -7,7 +7,7 @@ import ray
 
 
 @ray.remote
-class Level1(object):
+class Ranger(object):
     def __init__(self, stock_codes):
         with open(
             f"""{os.path.dirname(os.path.dirname(__file__))}/config/stock_config.json"""
@@ -15,46 +15,46 @@ class Level1(object):
             self.stock_config = json.load(f)
         self.stock_codes = stock_codes
         self.columns_names = list(self.stock_config["sina_datatype"].keys())
-        self.level1_dict = {
+        self.ranger_dict = {
             c: pd.DataFrame(columns=self.columns_names)
             .astype(self.stock_config["sina_datatype"])
             .set_index("time")
             for c in self.stock_codes
         }
 
-    def merge_new_level1_dict(self, new_dict_level1_lists):
+    def merge_new_ranger_dict(self, new_dict_ranger_lists):
         for k in self.stock_codes:
-            if len(new_dict_level1_lists[k]) >= 1:
+            if len(new_dict_ranger_lists[k]) >= 1:
                 new_df = pd.DataFrame(
-                    new_dict_level1_lists[k], columns=self.columns_names
+                    new_dict_ranger_lists[k], columns=self.columns_names
                 )
                 new_df = new_df.astype(self.stock_config["sina_datatype"]).set_index(
                     "time"
                 )
-                self.level1_dict[k] = pd.concat([self.level1_dict[k], new_df])
-                self.level1_dict[k]["volume_diff"] = self.level1_dict[k][
+                self.ranger_dict[k] = pd.concat([self.ranger_dict[k], new_df])
+                self.ranger_dict[k]["volume_diff"] = self.ranger_dict[k][
                     "volume"
                 ].diff()
-                self.level1_dict[k]["money_diff"] = self.level1_dict[k]["money"].diff()
+                self.ranger_dict[k]["money_diff"] = self.ranger_dict[k]["money"].diff()
 
-    def get_level1_dict_range(self, time_range_start_time, time_range_end_time):
+    def get_ranger_dict_range(self, time_range_start_time, time_range_end_time):
         return {
-            i: self.level1_dict[i][
-                (self.level1_dict[i].index >= time_range_start_time)
-                & (self.level1_dict[i].index < time_range_end_time)
+            i: self.ranger_dict[i][
+                (self.ranger_dict[i].index >= time_range_start_time)
+                & (self.ranger_dict[i].index < time_range_end_time)
             ]
-            for i in self.level1_dict
+            for i in self.ranger_dict
         }
 
-    def get_level1_dict_range_ticks2bar(
+    def get_ranger_dict_range_ticks2bar(
         self, time_range_start_time, time_range_end_time
     ):
-        level1_dict_range = self.get_level1_dict_range(
+        ranger_dict_range = self.get_ranger_dict_range(
             time_range_start_time, time_range_end_time
         )
 
         return {
-            i: level1_dict_range[i]
+            i: ranger_dict_range[i]
             .groupby("code")
             .aggregate(
                 open=pd.NamedAgg(column="current", aggfunc="first"),
@@ -65,7 +65,7 @@ class Level1(object):
                 money_range=pd.NamedAgg(column="money_diff", aggfunc="sum"),
             )
             .assign(time_range=f"{time_range_start_time},{time_range_end_time}")
-            if level1_dict_range[i].shape[0] > 0
+            if ranger_dict_range[i].shape[0] > 0
             else None
-            for i in level1_dict_range
+            for i in ranger_dict_range
         }
--- summergreen/schedulers/loaderscheduler.py
+++ summergreen/schedulers/loaderscheduler.py
@@ -2,6 +2,7 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from summergreen.loaders import sinastockloader
 import os
+import datetime
 
 sl = sinastockloader.SinaStockLoader(
     redis_host="localhost",
@@ -10,17 +11,17 @@ sl = sinastockloader.SinaStockLoader(
 )
 
 
-def redis2df_today2parquet(parquet_dir):
-    sl.redis2df_today().to_parquet(parquet_dir)
+def redis2df2parquet(date_str, parquet_dir):
+    sl.redis2df(date_str).to_parquet(parquet_dir)
 
 
 if __name__ == "__main__":
-
+    today_str = datetime.datetime.now().date()
     bs = BlockingScheduler()
     bs.add_job(
-        redis2df_today2parquet,
+        redis2df2parquet,
         "cron",
-        ["/mnt/stock_data/stock_tick_current_day/"],
+        [today_str, "/mnt/stock_data/stock_tick_current_day/"],
         hour="15",
         minute="1",
         second="1",
@@ -28,7 +29,6 @@ if __name__ == "__main__":
     )
 
     print("Press Ctrl+{0} to exit".format("Break" if os.name == "nt" else "C"))
-
     try:
         bs.start()
     except (KeyboardInterrupt, SystemExit):
